20efd6ebded612e43942faf3cb87d5421fc82d14
ddmitov <ddmitov@yahoo.com>
2018-02-25 20:47:24 +0200

    initial question rendering

--- CREDITS.md
+++ CREDITS.md
@@ -7,6 +7,7 @@ Patientia tua in studiis meis computatoriis auxilium meum maximum!
 http://mojolicious.org/perldoc/Mojolicious/Guides/Rendering  
 http://mojolicious.org/perldoc/Mojolicious/Controller#url_for  
 http://mojolicious.org/perldoc/Mojolicious/Command/daemon  
+http://mojolicious.org/perldoc/Mojo/Message/Request  
 
 http://blogs.perl.org/users/joel_berger/2012/10/a-websocket-mojoliciousdbi-example.html  
 http://blogs.perl.org/users/revlin_john/2013/03/re-simple-webserver---serve-static-files-with-mojolicious.html  
@@ -15,17 +16,25 @@ http://www.perlmonks.org/?node_id=1130920 (Mojolicious Websocket example)
 http://www.perlmonks.org/?node_id=1070002 (Mojolicious websocket with server-side repeating events)  
 http://www.perlmonks.org/?node_id=992521 (JSON::XS and unicode)  
 
-https://github.com/reyiyo/mojolicious_template  
-
-https://perlmaven.com/slurp  
-
-http://xmodulo.com/how-to-parse-json-string-in-perl.html  
-
 https://stackoverflow.com/questions/8258387/accessing-values-of-json-structure-in-perl  
 https://stackoverflow.com/questions/8463919/how-to-convert-a-simple-hash-to-json-in-perl  
 
+https://github.com/reyiyo/mojolicious_template  
+https://perlmaven.com/slurp  
+http://xmodulo.com/how-to-parse-json-string-in-perl.html  
+
+## HTML
+https://stackoverflow.com/questions/3216512/how-can-i-create-mutually-exclusive-radio-buttons-with-different-names  
+
 ## Bootstrap
 https://getbootstrap.com/docs/4.0/utilities/flex/  
+https://www.quackit.com/bootstrap/bootstrap_4/tutorial/bootstrap_cards.cfm  
+https://bootsnipp.com/snippets/featured/simple-css-custom-checkboxes-and-radios  
+
+## jQuery
+http://api.jquery.com/append/  
+https://api.jquery.com/empty/  
+https://www.w3schools.com/jquery/ajax_get.asp  
 
 ## Welcome Image
 https://commons.wikimedia.org/wiki/File%3ACoat_of_Arms_of_Bulgaria_XVII_century%2C_Insignia_procerum.jpg  
--- resources/app/mneme.js
+++ resources/app/mneme.js
@@ -1,4 +1,114 @@
+function insertQuestion(questionObject) {
+  var question = questionObject.question;
+  var variety = questionObject.class;
 
-function getFirstQuestion() {
-  webSocketConnection.send("get_question_1");
+  var variantOne = questionObject.a;
+  var variantTwo = questionObject.b;
+  var variantThree = questionObject.c;
+  var variantFour = questionObject.d;
+
+  var formElement = document.createElement("form");
+  formElement.setAttribute("name", "question");
+
+  var cardElement = document.createElement("div");
+  cardElement.setAttribute("class", "card card-body");
+
+  var cardTitleElement = document.createElement("h4");
+  cardTitleElement.setAttribute("class", "card-title");
+  cardTitleElement.innerHTML = question;
+  cardElement.appendChild(cardTitleElement);
+
+  var cardTextElement = document.createElement("p");
+  cardTextElement.setAttribute("class", "card-text");
+
+
+  var rowOne = document.createElement("div");
+  rowOne.setAttribute("class", "form-check");
+
+  var labelOne = document.createElement("label");
+  labelOne.setAttribute("class", "label-text");
+
+  var radioOne = document.createElement("input");
+  radioOne.setAttribute("type", "radio");
+  radioOne.setAttribute("name", "answer");
+  radioOne.setAttribute("id", "radioOne");
+
+  var spanOne = document.createElement("span");
+  spanOne.setAttribute("class", "label-text");
+  spanOne.innerHTML = " " + variantOne;
+
+  labelOne.appendChild(radioOne);
+  labelOne.appendChild(spanOne);
+  rowOne.appendChild(labelOne);
+  cardTextElement.appendChild(rowOne);
+
+
+  var rowTwo = document.createElement("div");
+  rowTwo.setAttribute("class", "form-check");
+
+  var labelTwo = document.createElement("label");
+  labelTwo.setAttribute("class", "label-text");
+
+  var radioTwo = document.createElement("input");
+  radioTwo.setAttribute("type", "radio");
+  radioTwo.setAttribute("name", "answer");
+  radioTwo.setAttribute("id", "radioTwo");
+
+  var spanTwo = document.createElement("span");
+  spanTwo.setAttribute("class", "label-text");
+  spanTwo.innerHTML = " " + variantTwo;
+
+  labelTwo.appendChild(radioTwo);
+  labelTwo.appendChild(spanTwo);
+  rowTwo.appendChild(labelTwo);
+  cardTextElement.appendChild(rowTwo);
+
+
+  var rowThree = document.createElement("div");
+  rowThree.setAttribute("class", "form-check");
+
+  var labelThree = document.createElement("label");
+  labelThree.setAttribute("class", "label-text");
+
+  var radioThree = document.createElement("input");
+  radioThree.setAttribute("type", "radio");
+  radioThree.setAttribute("name", "answer");
+  radioThree.setAttribute("id", "radioThree");
+
+  var spanThree = document.createElement("span");
+  spanThree.setAttribute("class", "label-text");
+  spanThree.innerHTML = " " + variantThree;
+
+  labelThree.appendChild(radioThree);
+  labelThree.appendChild(spanThree);
+  rowThree.appendChild(labelThree);
+  cardTextElement.appendChild(rowThree);
+
+
+  var rowFour = document.createElement("div");
+  rowFour.setAttribute("class", "form-check");
+
+  var labelFour = document.createElement("label");
+  labelFour.setAttribute("class", "label-text");
+
+  var radioFour = document.createElement("input");
+  radioFour.setAttribute("type", "radio");
+  radioFour.setAttribute("name", "answer");
+  radioFour.setAttribute("id", "radioFour");
+
+  var spanFour = document.createElement("span");
+  spanFour.setAttribute("class", "label-text");
+  spanFour.innerHTML = " " + variantFour;
+
+  labelFour.appendChild(radioFour);
+  labelFour.appendChild(spanFour);
+  rowFour.appendChild(labelFour);
+  cardTextElement.appendChild(rowFour);
+
+
+  cardElement.appendChild(cardTextElement);
+  formElement.appendChild(cardElement);
+
+  $('#question').empty();
+  $('#question').append(formElement);
 }
